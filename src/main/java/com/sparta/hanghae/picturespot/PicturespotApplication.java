75be23736b77225df9264dac58cc727ac177afda
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-04-23 23:43:27 +0900

    Initial commit

---
+++ src/main/java/com/sparta/hanghae/picturespot/PicturespotApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae.picturespot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturespotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicturespotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/hanghae/picturespot/PicturespotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae.picturespot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicturespotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
