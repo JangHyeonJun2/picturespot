ffdca697ced5187a212300e860628b4d9c357bb2
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-05-08 21:10:20 +0900

    UUID 변수 위치 변경 및 imgUrlId response에  추가

--- src/main/java/com/sparta/hanghae/picturespot/service/S3Service.java
+++ src/main/java/com/sparta/hanghae/picturespot/service/S3Service.java
@@ -50,6 +50,7 @@ public class S3Service {
 //            String fileName = uploadFile[i].getName().replace(" ", "");
             String fileName = uploadFile[i].getName().substring(uploadFile[i].getName().lastIndexOf('.'));
             Date date_now = new Date(System.currentTimeMillis()); // 현재시간을 가져와 Date형으로 저장한다
+            //파일 이름을 다르게 한다. 날짜로만헀는데 for문이 너무 빠르게 돌아서 mmss까지 커버가 안되서 교체!
             UUID uuid = UUID.randomUUID();
             String subUUID = uuid.toString().substring(0, 8);
             SimpleDateFormat fourteen_format = new SimpleDateFormat("yyyyMMddHHmmss");
