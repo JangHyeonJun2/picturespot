4681cdffddc69006d17b8b24e0dd0b3d7a008c28
sezeom <sejeong0516@gmail.com>
2021-05-10 12:13:34 +0900

    nick->id, 위도 경도 추가, 마이페이지 로그인 유무

--- src/main/java/com/sparta/hanghae/picturespot/dto/response/mypage/MypageResponseDto.java
+++ src/main/java/com/sparta/hanghae/picturespot/dto/response/mypage/MypageResponseDto.java
@@ -2,11 +2,11 @@ package com.sparta.hanghae.picturespot.dto.response.mypage;
 
 import com.sparta.hanghae.picturespot.dto.request.img.BoardImgCommonRequestDto;
 import com.sparta.hanghae.picturespot.model.Board;
-import com.sparta.hanghae.picturespot.model.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,17 +16,13 @@ import java.util.List;
 @NoArgsConstructor
 public class MypageResponseDto {
 
-    //user
-//    private String nickname;
-//    private String userImgUrl;
-//    private String userMsg;
-
     //board
     private Long boardId;
     private String writer;
     private String title;
     private String content;
-//    private String[] imgUrls;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
     private String category;
     private String spotName;
     private LocalDateTime modified;
@@ -42,10 +38,6 @@ public class MypageResponseDto {
 
     @Builder
     public MypageResponseDto(Board boardEntity, List<MypageCommentResponseDto> comments, boolean likeCheck, int likeCount, List<BoardImgCommonRequestDto> reponseDto) {
-        //user정보
-//        this.nickname = userEntity.getNickname();
-//        this.userImgUrl = userEntity.getImgUrl();
-//        this.userMsg = userEntity.getIntroduceMsg();
 
         //board 정보
         this.boardId = boardEntity.getId();
@@ -53,13 +45,12 @@ public class MypageResponseDto {
         this.title = boardEntity.getTitle();
         this.content = boardEntity.getContent();
         this.category = boardEntity.getCategory();
+        this.latitude = boardEntity.getLatitude();
+        this.longitude = boardEntity.getLongitude();
         this.spotName = boardEntity.getSpotName();
         this.modified = boardEntity.getModified();
-        //게시물 배열 초기화
-//        this.imgUrls = new String[boardEntity.getImgUrls().length];
-//        for (int i=0; i<boardEntity.getImgUrls().length; i++) {
-//            this.imgUrls[i] = boardEntity.getImgUrls()[i];
-//        }
+        //이미지
+        this.boardImgReponseDtoList = reponseDto;
 
         //좋아요
         this.liked = likeCheck;
@@ -67,13 +58,6 @@ public class MypageResponseDto {
 
         //댓글
         this.comments = comments;
-        //이미지
-        this.boardImgReponseDtoList = reponseDto;
+        }
     }
 
-//    public MypageResponseDto(User user){
-//        this.nickname = user.getNickname();
-//        this.userMsg = user.getIntroduceMsg();
-//        this.userImgUrl = user.getImgUrl();
-//    }
-}
