140d3f8d4e3d58054a92abac766b9400e62dfff8
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-05-20 19:45:27 +0900

    deploy 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl picturespot | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f picturespot | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
