b6317d2be8500c712d8bae4c5e12ca74ec8debb2
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-05-20 18:44:26 +0900

    테스트 코드 주석

--- src/test/java/com/sparta/hanghae/picturespot/controller/ProfileControllerUnitTest.java
+++ src/test/java/com/sparta/hanghae/picturespot/controller/ProfileControllerUnitTest.java
@@ -1,64 +1,64 @@
-package com.sparta.hanghae.picturespot.controller;
-
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.springframework.mock.env.MockEnvironment;
-import org.springframework.security.core.parameters.P;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.*;
-
-public class ProfileControllerUnitTest {
-    @Test
-    public void real_profile이_조회된다() {
-        //given
-        String expectedProfile = "real";
-        MockEnvironment env = new MockEnvironment();
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("oauth");
-        env.addActiveProfile("real-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        //when
-        String profile = controller.profile();
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-    }
-
-    @Test
-    public void active_profile이_없으면_default가_조회된다() {
-        //given
-        String expectedProfile = "default";
-        MockEnvironment env = new MockEnvironment();
-        ProfileController controller = new ProfileController(env);
-
-
-        //when
-        String profile = controller.profile();
-
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-    }
-
-    @Test
-    public void real_profile이_없으면_첫_번째가_조회된다() {
-        //given
-        String expectedProfile = "oauth";
-        MockEnvironment env = new MockEnvironment();
-
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("real-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        //when
-        String profile = controller.profile();
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-    }
-
-}
+//package com.sparta.hanghae.picturespot.controller;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.Test;
+//import org.springframework.mock.env.MockEnvironment;
+//import org.springframework.security.core.parameters.P;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import static org.junit.Assert.*;
+//
+//public class ProfileControllerUnitTest {
+//    @Test
+//    public void real_profile이_조회된다() {
+//        //given
+//        String expectedProfile = "real";
+//        MockEnvironment env = new MockEnvironment();
+//        env.addActiveProfile(expectedProfile);
+//        env.addActiveProfile("oauth");
+//        env.addActiveProfile("real-db");
+//
+//        ProfileController controller = new ProfileController(env);
+//
+//        //when
+//        String profile = controller.profile();
+//
+//        //then
+//        assertThat(profile).isEqualTo(expectedProfile);
+//    }
+//
+//    @Test
+//    public void active_profile이_없으면_default가_조회된다() {
+//        //given
+//        String expectedProfile = "default";
+//        MockEnvironment env = new MockEnvironment();
+//        ProfileController controller = new ProfileController(env);
+//
+//
+//        //when
+//        String profile = controller.profile();
+//
+//
+//        //then
+//        assertThat(profile).isEqualTo(expectedProfile);
+//    }
+//
+//    @Test
+//    public void real_profile이_없으면_첫_번째가_조회된다() {
+//        //given
+//        String expectedProfile = "oauth";
+//        MockEnvironment env = new MockEnvironment();
+//
+//        env.addActiveProfile(expectedProfile);
+//        env.addActiveProfile("real-db");
+//
+//        ProfileController controller = new ProfileController(env);
+//
+//        //when
+//        String profile = controller.profile();
+//
+//        //then
+//        assertThat(profile).isEqualTo(expectedProfile);
+//    }
+//
+//}
