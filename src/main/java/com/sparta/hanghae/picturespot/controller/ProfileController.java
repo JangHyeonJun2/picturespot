c9776dd1d9e4bb56a4b235597fb36ad9553b2430
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-05-20 18:55:13 +0900

    코드 수정

--- src/main/java/com/sparta/hanghae/picturespot/controller/ProfileController.java
+++ src/main/java/com/sparta/hanghae/picturespot/controller/ProfileController.java
@@ -19,9 +19,10 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real", "real1","real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        return profiles.stream()
-                .filter(realProfiles::contains)
-                .findAny()
-                .orElse(defaultProfile);
+//        return profiles.stream()
+//                .filter(realProfiles::contains)
+//                .findAny()
+//                .orElse(defaultProfile);
+        return "String";
     }
 }
