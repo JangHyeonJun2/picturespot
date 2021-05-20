d54127e9e6a6e43bb7947335062feac7a2c6fe37
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-05-21 04:35:44 +0900

    메인 실행문 코드 수정4

--- src/main/java/com/sparta/hanghae/picturespot/PicturespotApplication.java
+++ src/main/java/com/sparta/hanghae/picturespot/PicturespotApplication.java
@@ -24,16 +24,16 @@ public class PicturespotApplication {
 //    }
 //
     //main 실행문
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:application-real.yml,";
+//    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//            + "classpath:application.yml,"
+//            + "classpath:application-real.yml,";
 
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(PicturespotApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-//        SpringApplication.run(PicturespotApplication.class, args);
+//        new SpringApplicationBuilder(PicturespotApplication.class)
+//                .properties(APPLICATION_LOCATIONS)
+//                .run(args);
+        SpringApplication.run(PicturespotApplication.class, args);
 
     }
 //    @PostConstruct
